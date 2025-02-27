e36748f983d58367d77b8daac23e59e083b42183
Арсений <sene4ka.abrosimov@yandex.ru>
2025-02-28 00:52:25 +0300

    refactor: update algorithm + fix bug

--- split_sum.py
+++ split_sum.py
@@ -5,15 +5,15 @@ import pandas as pd
 from scipy.signal import find_peaks
 
 def fft_split(data_x, data_y):
-    freq_spectre = fft(data_y)
+    spectre = fft(data_y)
 
     fs = int(1 / np.mean(np.diff(data_x)))
     
-    frequencies = fftfreq(len(data_x), 1 / fs)
+    full_frequencies = fftfreq(len(data_x), 1 / fs)
 
-    freq_spectre = np.abs(freq_spectre[:len(frequencies) // 2])
+    freq_spectre = np.abs(spectre[:len(full_frequencies) // 2].copy())
     
-    frequencies = frequencies[:len(frequencies) // 2]
+    frequencies = full_frequencies[:len(full_frequencies) // 2].copy()
 
     peaks, peaks_dict = find_peaks(freq_spectre, height=10)
 
@@ -23,23 +23,35 @@ def fft_split(data_x, data_y):
     max1i, max2i = -1, -1
     for i in range(len(peaks_data)):
         if peaks_data[i] > max1:
+            if max1 > max2:
+                max2 = max1
+                max2i = max1i
             max1 = peaks_data[i]
             max1i = peaks[i]
         elif peaks_data[i] > max2:
             max2 = peaks_data[i]
             max2i = peaks[i]
 
+    if max1i > max2i:
+        max1i, max2i = max2i, max1i
+
     if max1 - max2 > max2:
         max2i = max1i
     elif max2 - max1 > max1:
         max1i = max2i
 
-    return frequencies[max1i], frequencies[max2i]
+    spectre_1 = spectre.copy()
+    spectre_1[np.abs(full_frequencies) != frequencies[max1i]] = 0
+
+    spectre_2 = spectre.copy()
+    spectre_2[np.abs(full_frequencies) != frequencies[max2i]] = 0
+    
+    return ifft(spectre_1).real, ifft(spectre_2).real
 
     
 
 location = "./data/sum"
-filename = "DS0007.csv"
+filename = "DS0005.csv"
 path = location + '/' + filename
 
 pd.options.display.max_rows = 10000
@@ -58,9 +70,7 @@ plt.title("Оригинальный сигнал 1 + 2")
 plt.legend()
 plt.grid()
 
-freq_1, freq_2 = fft_split(signal_x, signal_y)
-
-signal_1_y = [np.sin(2 * np.pi * freq_1 * i) for i in signal_x]
+signal_1_y, signal_2_y = fft_split(signal_x, signal_y)
 
 plt.subplot(3, 1, 2)
 plt.plot(signal_x, signal_1_y, label="Сигнал")
@@ -69,8 +79,6 @@ plt.ylabel("Амплитуда, В")
 plt.title("Сигнал 1")
 plt.legend()
 plt.grid()
-
-signal_2_y = [np.sin(2 * np.pi * freq_2 * i) for i in signal_x]
 
 plt.subplot(3, 1, 3)
 plt.plot(signal_x, signal_2_y, label="Сигнал")
